7d4008a0e00d53431d582f659ec4c615d3d386c5
ericoborgonove <aluno@UNN022-L004-001.serlab.intra>
2026-02-24 21:18:08 -0400

    feat: servidor - dotenv

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+require('dotenv').config()
+
 const express = require ('express');
+const path = require('path');
 const app = express();
-const PORT = 3000;
+//const PORT = 3000;
+
+app.get ('/mussum', (req, res) => {
+    res.sendFile(path.resolve('algo.html'))
+})
 
 app.get ('/', (req, res) => {
     res.send ('Hello World')
@@ -12,11 +19,13 @@ app.get ('/oi', (req, res) => {
 
 // são os ultimos !!!!!
 app.use((req, res, next) => {
-    res.status(404).send("Rota não encontrada.")
+    //res.status(404).send("Rota não encontrada.")
+    res.status(404).sendFile(path.resolve('erro404.html'))
 })
 
-app.listen (PORT, ()=> {
-    console.log (`App rodando na porta ${PORT}`)
+//app.listen (PORT, ()=> {
+    app.listen (process.env.PORT, ()=> {
+    console.log (`App rodando na porta ${process.env.PORT}`)
 })
 
 //https://github.com/EricoBorgonove/UNN_ADS_4P_BACK
