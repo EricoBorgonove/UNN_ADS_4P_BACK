13349842cc0fe314eead0c9291b4d3d4b6ffa8ec
ericoborgonove <aluno@UNN022-L004-001.serlab.intra>
2026-02-24 20:29:17 -0400

    doc: atualizaÇÃO

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const PORT = 3000;
 app.get ('/', (req, res) => {
     res.send ('Hello World')
 })
+
 app.get ('/oi', (req, res) => {
     res.send ('oi')
+})
+
+// são os ultimos !!!!!
+app.use((req, res, next) => {
+    res.status(404).send("Rota não encontrada.")
 })
 
 app.listen (PORT, ()=> {
     console.log (`App rodando na porta ${PORT}`)
 })
+
+//https://github.com/EricoBorgonove/UNN_ADS_4P_BACK
